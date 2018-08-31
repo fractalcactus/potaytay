e115f398138915e9a3a7bf0ff8be6d1110dac591
fractalcactus <johndoe@example.com>
2018-08-31 18:34:14 +1200

    configure db for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_08_31_022848) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "memories", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
