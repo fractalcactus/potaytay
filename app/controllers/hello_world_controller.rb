34431d242b94f91e95505f561c5cd4da55d6b574
fractalcactus <johndoe@example.com>
2018-09-01 11:46:27 +1200

    style memory

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -4,6 +4,6 @@ class HelloWorldController < ApplicationController
   layout "hello_world"
 
   def index
-    @memory_props = Memory.all.as_json(:only => [:top, :left, :text])
+    @memory_props = Memory.all.as_json(:only => [:top, :left, :text, :image])
   end
 end
