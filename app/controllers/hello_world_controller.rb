39c613fa428290c4d08b08cb5fc80b0133cc7e47
fractalcactus <johndoe@example.com>
2018-09-09 14:48:23 +1200

    move quotes to envs

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -6,5 +6,28 @@ class HelloWorldController < ApplicationController
 
   def index
     @memory_props = Memory.all.as_json(:only => [:top, :left, :text, :image])
+    @quote_props =  generate_quote_props
+  end
+
+  private
+
+  def generate_quote_props
+    {     jasper:   eval(ENV["jasper"]),
+          brigette: eval(ENV["brigette"]),
+          andrew:   eval(ENV["andrew"]),
+          louise:   eval(ENV["louise"]),
+          will:     eval(ENV["will"]),
+          thijs:    eval(ENV["thijs"]),
+          daniel:   eval(ENV["daniel"]),
+          gabby:    eval(ENV["gabby"]),
+          rose:     eval(ENV["rose"]),
+          eden:     eval(ENV["eden"]),
+          ollie:    eval(ENV["ollie"]),
+          harry:    eval(ENV["harry"]),
+          bella:    eval(ENV["bella"]),
+          will:     eval(ENV["will"]),
+          india:    eval(ENV["india"]),
+          anna:     eval(ENV["anna"])
+    }
   end
 end
